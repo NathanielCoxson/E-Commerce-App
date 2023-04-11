e76ea96cb773931a25d42233e7f4ea5caca588da
Nathan Coxson <nathancoxson2739@gmail.com>
2023-04-10 21:25:47 -0400

    Added login functionality which also includes the addition of express sesssions.

--- server/queries.js
+++ server/queries.js
@@ -11,33 +11,78 @@ const saltRounds = 10;
 
 const registerUser = (req, res) => {
     let username = req.body.username;
-    let usernameTaken = false;
     pool.query('SELECT * FROM users WHERE username = $1', [username], (error, result) => {
-        if (result.rows != 0) {
-            usernameTaken = true;
+        if (error) {
+            console.log(error);
         }
-    });
-    if (usernameTaken) {
-        res.status(400).send("Username already taken");
-    }
-    else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-            pool.query(
-                'INSERT INTO users (username, hashed_password) VALUES($1, $2)', 
-                [username, hash], 
-                (error, result) => {
-                    if (error) {
-                        throw error;
-                        res.status(400).send('Error creating new user.');
-                    }
-                    else {
-                        res.status(201).send(`Added new user with username: ${req.body.username}.`);
-                    }
+        else if (result.rows != 0) {
+            res.status(400).send('Username already taken.');
+        }
+        else {
+            bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+                pool.query(
+                    'INSERT INTO users (username, hashed_password) VALUES($1, $2)', 
+                    [username, hash], 
+                    (error, result) => {
+                        if (error) {
+                            console.log(error);
+                            res.status(400).send('Error creating new user.');
+                        }
+                        else {
+                            res.status(201).send(`Added new user with username: ${req.body.username}.`);
+                        }
+                });
             });
+        } 
+    });
+     
+}
+
+const loginUser = (req, res) => {
+    res.send('Login Successful.');
+}
+
+/*
+    Verification function used by Passport.js to authenticate a user given
+    their username and password. The function queries the DB to find the 
+    correct user by the username and then compares the hashed password with
+    the password provided.
+*/
+function verify(username, password, cb) {
+    pool.query('SELECT * FROM users WHERE username = $1', [username], (error, result) => {
+        if (error) {
+            return cb(error);
+        }
+        if (result.rowCount === 0) {
+            return cb(null, false, {message: 'Incorrect username or password.'});
+        }
+        bcrypt.compare(password, result.rows[0].hashed_password, (err, correct) => {
+            if (err) {
+                return cb(err);
+            }
+            if (!correct) {
+                return cb(null, false, {message: 'Incorrect username or password.'});
+            }
+            return cb(null, result.rows[0]);
         });
-    }  
+    });
+}
+
+const findUserById = id => {
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, result) => {
+        if (error) {
+            throw error;
+        }
+        if (result.rowCount === 0) {
+            return {};
+        }
+        return result.rows[0];
+    })
 }
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser,
+    verify,
+    findUserById
 }
--- server/server.js
+++ server/server.js
@@ -1,18 +1,43 @@
 const express = require('express');
+const app = express();
+const session = require('express-session');
 const db = require('./queries.js');
 const bodyParser = require('body-parser');
-
-const app = express();
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(session({
+        secret: "f4z4gs$Gcg",
+        cookie: {maxAge: 300000000, secure: true},
+        saveUninitialized: false,
+        resave: false,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    return done(null, id);
+});
+passport.use(new LocalStrategy(db.verify));
 
 //Endpoints
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 app.post('/register', db.registerUser);
+app.post('/login', 
+    passport.authenticate('local', {failureRedirect: '/', failureMessage: true}),
+    db.loginUser
+);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
